24aea53fcdd2d1b3b66784507f1799a9fdfaf21f
Patrick Salguero <patrick.salguero.avalos@gmail.com>
2018-04-14 09:26:37 -0500

    Se agrega template ionic-starter-3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,87 @@
-import { NgModule, ErrorHandler } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
+import {NgModule} from "@angular/core";
+import {IonicApp, IonicModule} from "ionic-angular";
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
+import {IonicStorageModule} from '@ionic/storage';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
+import {StatusBar} from '@ionic-native/status-bar';
+import {SplashScreen} from '@ionic-native/splash-screen';
+import {Keyboard} from '@ionic-native/keyboard';
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import {ActivityService} from "../services/activity-service";
+import {TripService} from "../services/trip-service";
+import {WeatherProvider} from "../services/weather";
+
+import {MyApp} from "./app.component";
+
+import {SettingsPage} from "../pages/settings/settings";
+import {CheckoutTripPage} from "../pages/checkout-trip/checkout-trip";
+import {HomePage} from "../pages/home/home";
+import {LoginPage} from "../pages/login/login";
+import {NotificationsPage} from "../pages/notifications/notifications";
+import {RegisterPage} from "../pages/register/register";
+import {SearchLocationPage} from "../pages/search-location/search-location";
+import {TripDetailPage} from "../pages/trip-detail/trip-detail";
+import {TripsPage} from "../pages/trips/trips";
+import {LocalWeatherPage} from "../pages/local-weather/local-weather";
+
+// import services
+// end import services
+// end import services
+
+// import pages
+// end import pages
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
+    SettingsPage,
+    CheckoutTripPage,
     HomePage,
-    TabsPage
+    LoginPage,
+    LocalWeatherPage,
+    NotificationsPage,
+    RegisterPage,
+    SearchLocationPage,
+    TripDetailPage,
+    TripsPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    HttpClientModule,
+    IonicModule.forRoot(MyApp, {
+      scrollPadding: false,
+      scrollAssist: true,
+      autoFocusAssist: false
+    }),
+    IonicStorageModule.forRoot({
+      name: '__ionic3_start_theme',
+        driverOrder: ['indexeddb', 'sqlite', 'websql']
+    })
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
+    SettingsPage,
+    CheckoutTripPage,
     HomePage,
-    TabsPage
+    LoginPage,
+    LocalWeatherPage,
+    NotificationsPage,
+    RegisterPage,
+    SearchLocationPage,
+    TripDetailPage,
+    TripsPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    Keyboard,
+    ActivityService,
+    TripService,
+    WeatherProvider
   ]
 })
-export class AppModule {}
+
+export class AppModule {
+}
